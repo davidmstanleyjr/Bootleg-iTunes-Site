465120383ccd1eb6205bc9d4a2f27243f36a8a5c
David Stanley <davidmstanleyjr@gmail.com>
2020-05-16 15:17:13 -0400

    started javascript. I just set up my selectors for getting elements on the page.

---
+++ js/app.js
@@ -0,0 +1,7 @@
+//Selector's for getting elements on the page.
+
+const container = document.querySelector('.container');
+const artistImage = document.querySelector('.artist');
+const media = document.querySelector('.media');
+const overlay = document.querySelector('.overlay');
+const searchElem = document.querySelector('#search');
