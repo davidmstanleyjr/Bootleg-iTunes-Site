d81468531a69795f2253ec05da841dc35c54b870
David Stanley <davidmstanleyjr@gmail.com>
2020-05-16 18:19:34 -0400

    created logic to retrieve data from iTunes

--- js/app.js
+++ js/app.js
@@ -5,3 +5,13 @@ const artistImage = document.querySelector('.artist');
 const media = document.querySelector('.media');
 const overlay = document.querySelector('.overlay');
 const searchElem = document.querySelector('#search');
+
+//Retrieve data from iTunes
+const getContent = (search) => {
+	const url = new URL('https://itunes.apple.com/search');
+	const params = { term: search, media: 'musicVideo' };
+	url.search = new URLSearchParams(params);
+	fetch(url, { method: 'POST' }).then((results) => results.json()).then((data) => {});
+};
+
+getContent('beyonce');
