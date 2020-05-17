ef8f39d99ad7bb0b43156e8a5273cf9d1701d56e
David Stanley <davidmstanleyjr@gmail.com>
2020-05-16 20:29:19 -0400

    wrote a varaible that gets the conten from the itunes API

--- js/app.js
+++ js/app.js
@@ -7,11 +7,25 @@ const overlay = document.querySelector('.overlay');
 const searchElem = document.querySelector('#search');
 
 //Retrieve data from iTunes
+searchElem.addEventListener('keydown', (event) => {
+	if (event.key === 'Enter') {
+		getContent(searchElem.value);
+	}
+});
+
 const getContent = (search) => {
 	const url = new URL('https://itunes.apple.com/search');
 	const params = { term: search, media: 'musicVideo' };
 	url.search = new URLSearchParams(params);
-	fetch(url, { method: 'POST' }).then((results) => results.json()).then((data) => {});
+	fetch(url, { method: 'POST' }).then((results) => results.json()).then((data) => {
+		const resultsHTML = data.results.map(
+			(result) => `
+        <div style="background-image: url(${result.artworkUrl100});"
+        onclick="openMedia('${result.previewURl}', '${result.trackCensoredName}
+        ')" class="result"></div>
+        `
+		);
+	});
 };
 
 getContent('beyonce');
